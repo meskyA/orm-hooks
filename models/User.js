dadc26b1f97ae323b04d1fc63413bb3d7c922b1e
meskyA <mesky2015@gmail.com>
2022-09-11 07:00:33 -0700

    rearranged file structure

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../../orm-hooks/config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {}
